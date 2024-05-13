b98ba954fe2e7ef27b1b8e1424cfd6fb3abad467
alchemistsGestalt <zeinhatem@proton.me>
2024-05-13 21:24:06 +0300

    I realised that I didn't push ash.hpp
    I just added two header files that will probably be used by the whole project.

--- src/ash.hpp
+++ src/ash.hpp
@@ -3,7 +3,6 @@
  * Copyright (C) 2024 First Time Compile
  *
  * Author: alchemistsGestalt
- * Maintainer: alchemistsGestalt/Darth-218/anyone else that wants to
  * Created: 13 May, 2024
  * Modified: 13 May, 2024
  * Homepage: https://github.com/darth-218/ash
@@ -14,7 +13,8 @@
 #ifndef ASH_H_
 #define ASH_H_
 
-
+#include <iostream>
+#include <string>
 
 #endif // ASH_H_
 
