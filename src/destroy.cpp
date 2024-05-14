8ff4e59f1e2a0a337b91104a7c4ce71c563a5e0b
darth-218 <yahia.hany.gaber@proton.me>
2024-05-14 12:23:20 +0300

    Checks if a directory is being deleted and prompt the user for confirmation

--- src/destroy.cpp
+++ src/destroy.cpp
@@ -1,11 +1,25 @@
+#include <cstdio>
 #include <filesystem>
+#include <iostream>
 
 using namespace std;
+
+// TODO: Add a verbose parameter
+// TODO: Add a force parameter
 
 int main(int args, char **argv) {
 
   for (int arg = 1; arg < args; arg++) {
 
-    filesystem::remove(argv[arg]);
+    if (!(filesystem::is_directory(argv[arg])))
+      filesystem::remove(argv[arg]);
+    else {
+      cout << "Deleting a directory, do you want to proceed? [y/N] ";
+      char confirmation = getchar();
+      if (tolower(confirmation) == 'y')
+        filesystem::remove_all(argv[arg]);
+      else
+        continue;
+    }
   }
 }
